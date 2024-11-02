ef21ad7c83db879efa6e32e7c5db5daf36bcc68d
Matthew A. Davis <mdavis@fhrp.org>
2024-11-02 09:05:43 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mdavistffhrtporg/fhrp-pubtheme
+
+go 1.22.8
+
+require (
+	github.com/FortAwesome/Font-Awesome v4.7.0+incompatible // indirect
+	github.com/twbs/bootstrap v5.3.3+incompatible // indirect
+)
---
+++ layouts/partials/scripts/js/ga.js
@@ -0,0 +1,10 @@
+var imported = document.createElement('script');
+imported.src = 'https://www.googletagmanager.com/gtag/js?id=AW-11438522763';
+document.head.appendChild(imported);
+
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'AW-11438522763');
